c5a9a4db84d56244a7e640516cf093e5ed989851
saisharanya11 <saisharanyakashoju@gmail.com>
2025-01-29 16:37:06 +0530

    Add files via upload

---
+++ SDBMS.java
@@ -0,0 +1,12 @@
+package com.tyss.SDBMS.controller;
+
+public interface SDBMS {
+
+	void addStudent();
+	void searchStudent();
+	void displayAllStudent();
+	void countStudent();
+	void sortStudent();
+	void removeStudent();
+	void updateStudentMarks();
+}
